fad2ae64fa97057710715a15565735d892a39d47
Abdullah Yassine <146992967+abdullah-yassine@users.noreply.github.com>
2026-06-22 13:43:12 -0600

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        firstPtr = secondPtr = 0
+        result = []
+        nums1Cp = nums1.copy()
+        nums2Cp = nums2.copy()
+        while firstPtr < len(nums1Cp) - n and secondPtr < len(nums2Cp) :
+            if nums1Cp[firstPtr] < nums2Cp[secondPtr]:
+                nums1[firstPtr+secondPtr] = nums1Cp[firstPtr] 
+                firstPtr += 1
+            else:
+                nums1[firstPtr+secondPtr] = nums2Cp[secondPtr]
+                secondPtr += 1
+            
+        while firstPtr < len(nums1Cp) - n :
+            nums1[firstPtr+secondPtr] = nums1Cp[firstPtr] 
+            firstPtr += 1
+            
+        while secondPtr < len(nums2Cp) :
+            nums1[firstPtr+secondPtr] = nums2Cp[secondPtr]
+            secondPtr += 1
+            
+        
+       
+        
