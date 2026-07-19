430a4c7df736f84aa721f2aabb3082f9acff8aff
Abdullah Yassine <146992967+abdullah-yassine@users.noreply.github.com>
2026-07-19 12:53:45 -0600

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        degrees = [0] * numCourses
+        adjacency_list = defaultdict(list) # Course : courses that depend on Course
+        for dependent, independent in prerequisites:
+            degrees[dependent] += 1
+            adjacency_list.setdefault(independent, set())
+            adjacency_list[independent].add(dependent)
+        q = deque() # bfs
+        for course, degree in enumerate(degrees):
+            if degree  == 0:
+                q.append(course)
+        order = []
+        while q:
+            pop = q.popleft()
+            for course in adjacency_list.get(pop, set()):
+                degrees[course] -= 1
+                if degrees[course] == 0:
+                    q.append(course)
+            order.append(pop)
+        return order if len(order) == numCourses else []
